4bad642046fb69bf3e4939acd0784aa4059db9cf
zclimber <mywork-olymps@yandex.ru>
2017-04-11 00:44:19 +0300

    Size optimizations implemented - v.1.1 getter functions min(), max() changed vEB tree<1> to 1 byte version

--- vebtree/veb.cpp
+++ vebtree/veb.cpp
@@ -14,8 +14,8 @@ namespace {
 template<unsigned int S>
 class int_type_selector {
 public:
-	using type = unsigned long long;
-//	using type = typename std::conditional<S <= 31, unsigned int, unsigned long long>::type;
+//	using type = unsigned long long;
+	using type = typename std::conditional<S <= 31, unsigned int, unsigned long long>::type;
 };
 }
 
@@ -25,39 +25,53 @@ private:
 	template<unsigned int T>
 	friend class VEBTree;
 	using int_type = typename int_type_selector<S>::type;
+	using int_type_low = typename int_type_selector<S / 2>::type;
+	using int_type_high = typename int_type_selector<(S + 1) / 2>::type;
 	constexpr const static int_type NOPE = (int_type) NO;
-	int_type min = NOPE, max = NOPE;
+	int_type __min = NOPE, __max = NOPE;
 	VEBTree<(S + 1) / 2> aux;
 	std::unordered_map<int_type, VEBTree<S / 2>> children;
+	int_type min() const {
+		return __min;
+	}
+	int_type & min() {
+		return __min;
+	}
+	int_type max() const {
+		return __max;
+	}
+	int_type & max() {
+		return __max;
+	}
 	bool empty() const {
-		return (min == NOPE);
+		return (min() == NOPE);
 	}
-	static int_type high(const int_type x) {
-		return (x >> (S / 2));
+	static int_type_high high(const int_type x) {
+		return int_type_high(x >> (S / 2));
 	}
-	constexpr const static int_type low_mask = (1ULL << (S / 2)) - 1;
-	static int_type low(const int_type x) {
+	constexpr const static int_type_low low_mask = (1ULL << (S / 2)) - 1;
+	static int_type_low low(const int_type x) {
 		return x & low_mask;
 	}
 	void inner_add(const int_type x) {
-		if (min == x || max == x) {
+		if (min() == x || max() == x) {
 			return;
 		}
 		if (empty()) {
-			min = max = x;
-		} else if (min == max) {
-			if (x > max) {
-				max = x;
+			min() = max() = x;
+		} else if (min() == max()) {
+			if (x > max()) {
+				max() = x;
 			} else {
-				min = x;
+				min() = x;
 			}
-		} else if (min > x) {
-			int_type old_min = min;
-			min = x;
+		} else if (min() > x) {
+			int_type old_min = min();
+			min() = x;
 			inner_add(old_min);
-		} else if (max < x) {
-			int_type old_max = max;
-			max = x;
+		} else if (max() < x) {
+			int_type old_max = max();
+			max() = x;
 			inner_add(old_max);
 		} else {
 			VEBTree<S / 2> & lower_tree = children[high(x)];
@@ -78,37 +92,39 @@ private:
 	}
 
 	void inner_remove(int_type x) {
-		if (empty() || x < min || x > max) {
+		if (empty() || x < min() || x > max()) {
 			return;
 		}
-		if (min == x && max == x) {
-			min = max = NOPE;
-		} else if (min == x) {
+		if (min() == x && max() == x) {
+			min() = max() = NOPE;
+		} else if (min() == x) {
 			if (aux.empty()) {
-				min = max;
+				min() = max();
 			} else {
-				assert(children.count(aux.min));
-				VEBTree<S / 2> * lower_tree = &(children.find(aux.min)->second);
-				int_type lower_min = lower_tree->min;
-				min = (aux.min << (S / 2)) + lower_min;
+				assert(children.count(aux.min()));
+				VEBTree<S / 2> * lower_tree =
+						&(children.find(aux.min())->second);
+				int_type_low lower_min = lower_tree->min();
+				min() = (aux.min() << (S / 2)) + lower_min;
 				lower_tree->inner_remove(lower_min);
 				if (lower_tree->empty()) {
-					children.erase(aux.min);
-					aux.inner_remove(aux.min);
+					children.erase(aux.min());
+					aux.inner_remove(aux.min());
 				}
 			}
-		} else if (max == x) {
+		} else if (max() == x) {
 			if (aux.empty()) {
-				max = min;
+				max() = min();
 			} else {
-				assert(children.count(aux.max));
-				VEBTree<S / 2> * lower_tree = &(children.find(aux.max)->second);
-				int_type lower_max = lower_tree->max;
-				max = (aux.max << (S / 2)) + lower_max;
+				assert(children.count(aux.max()));
+				VEBTree<S / 2> * lower_tree =
+						&(children.find(aux.max())->second);
+				int_type_low lower_max = lower_tree->max();
+				max() = (aux.max() << (S / 2)) + lower_max;
 				lower_tree->inner_remove(lower_max);
 				if (lower_tree->empty()) {
-					children.erase(aux.max);
-					aux.inner_remove(aux.max);
+					children.erase(aux.max());
+					aux.inner_remove(aux.max());
 				}
 			}
 		} else {
@@ -131,62 +147,62 @@ private:
 //			assert(children.count(elem));
 //		}
 	}
-	unsigned long long inner_next(int_type x) const {
-		if (x >= max) {
-			return NO;
-		} else if (x < min) {
-			return min;
+	int_type inner_next(int_type x) const {
+		if (x >= max()) {
+			return NOPE;
+		} else if (x < min()) {
+			return min();
 		} else if (aux.empty()) {
-			return max;
+			return max();
 		} else {
 			auto lower_tree_it = children.find(high(x));
 			if (lower_tree_it != children.end()
-					&& lower_tree_it->second.max > low(x)) {
-				return (high(x) << (S / 2))
+					&& lower_tree_it->second.max() > low(x)) {
+				return (int_type(high(x)) << (S / 2))
 						+ lower_tree_it->second.inner_next(low(x));
 			} else {
 				int_type next_high = aux.inner_next(high(x));
-				if (next_high == NO) {
-					return max;
+				if (next_high == aux.NOPE) {
+					return max();
 				}
 				if (children.count(next_high) == 0) {
 					std::cerr << "in vebtree" << S << " accessed index "
 							<< next_high << " from " << x << "\n";
 				}
-				return (next_high << (S / 2)) + children.at(next_high).min;
+				return (next_high << (S / 2)) + children.at(next_high).min();
 			}
 		}
 	}
-	unsigned long long inner_prev(int_type x) const {
-		if (x <= min) {
-			return NO;
-		} else if (x > max) {
-			return max;
+	int_type inner_prev(int_type x) const {
+		if (x <= min()) {
+			return NOPE;
+		} else if (x > max()) {
+			return max();
 		} else if (aux.empty()) {
-			return min;
+			return min();
 		} else {
 			auto lower_tree_it = children.find(high(x));
 			if (lower_tree_it != children.end()
-					&& lower_tree_it->second.min < low(x)) {
-				return (high(x) << (S / 2))
+					&& lower_tree_it->second.min() < low(x)) {
+				return (int_type(high(x)) << (S / 2))
 						+ lower_tree_it->second.inner_prev(low(x));
 			} else {
 				int_type prev_high = aux.inner_prev(high(x));
-				if (prev_high == NO) {
-					return min;
+				if (prev_high == aux.NOPE) {
+					return min();
 				}
 				if (children.count(prev_high) == 0) {
 					std::cerr << "in vebtree" << S << " accessed index "
 							<< prev_high << " from " << x << "\n";
 				}
-				return (prev_high << (S / 2)) + children.at(prev_high).max;
+				return (prev_high << (S / 2)) + children.at(prev_high).max();
 			}
 		}
 	}
 	void to_set(std::set<unsigned long long> &res,
 			unsigned long long high_part) const {
-		res.insert(high_part + min);
-		res.insert(high_part + max);
+		res.insert(high_part + min());
+		res.insert(high_part + max());
 		for (auto child : children) {
 			child.second.to_set(res, high_part + (child.first << (S / 2)));
 		}
@@ -215,74 +231,93 @@ public:
 		if (x >= (1ULL << S)) {
 			return NO;
 		} else {
-			return inner_next(x);
+			int_type inext = inner_next(x);
+			return inext == NOPE ? NO : inext;
 		}
 	}
 
 	unsigned long long prev(unsigned long long x) const {
 		if (x >= (1ULL << S)) {
-			return max == NOPE ? NO : max;
+			return max() == NOPE ? NO : max();
 		} else {
-			return inner_prev(x);
+			int_type iprev = inner_prev(x);
+			return iprev == NOPE ? NO : iprev;
 		}
 	}
 
 	unsigned long long getMin() const {
-		return min == NOPE ? NO : min;
+		return min() == NOPE ? NO : min();
 	}
 
 	unsigned long long getMax() const {
-		return max == NOPE ? NO : max;
+		return max() == NOPE ? NO : max();
 	}
 };
 
 template<>
 class VEBTree<1> {
+	template<unsigned int T>
+	friend class VEBTree;
 	using int_type = typename int_type_selector<0>::type;
 	constexpr const static int_type NOPE = (int_type) NO;
-public:
-	int_type min = NOPE, max = NOPE;
+	char bits = 0;
+	int_type min() const {
+		switch (bits) {
+		case 0:
+			return NOPE;
+		case 1:
+		case 3:
+			return 0;
+		case 2:
+			return 1;
+		default:
+			return NOPE;
+		}
+	}
+	int_type max() const {
+		switch (bits) {
+		case 0:
+			return NOPE;
+		case 2:
+		case 3:
+			return 1;
+		case 1:
+			return 0;
+		default:
+			return NOPE;
+		}
+	}
 	bool empty() const {
-		return (min == NOPE);
+		return (bits == 0);
 	}
 	void inner_add(const int_type x) {
-		if (empty()) {
-			min = max = x;
-		} else if (min == max) {
-			if (x > max) {
-				max = x;
-			} else {
-				min = x;
-			}
-		}
+		bits |= char(1 << x);
 	}
 	void inner_remove(int_type x) {
-		if (min == x && max == x) {
-			min = max = NOPE;
-		} else if (min == x) {
-			min = max;
-		} else if (max == x) {
-			max = min;
-		}
+		bits &= char(~(1 << x));
 	}
-	unsigned long long inner_next(int_type x) const {
-		if (max == 1 && x == 0) {
+	int_type inner_next(int_type x) const {
+		if (x == 0 && (bits & 2)) {
 			return 1;
 		} else {
-			return NO;
+			return NOPE;
 		}
 	}
-	unsigned long long inner_prev(int_type x) const {
-		if (min == 0 && x == 1) {
+	int_type inner_prev(int_type x) const {
+		if (x == 1 && (bits & 1)) {
 			return 0;
 		} else {
-			return NO;
+			return NOPE;
 		}
 	}
 	void to_set(std::set<unsigned long long> &res,
 			unsigned long long high_part) const {
-		res.insert(high_part + min);
-		res.insert(high_part + max);
+		if (bits & 1) {
+			res.insert(high_part);
+		}
+		if (bits & 2) {
+			res.insert(high_part + 1);
+		}
 	}
 };
 
@@ -312,13 +347,27 @@ int main() {
 			events.append("\n");
 		}
 		bool test_rs = true;
-		std::set<unsigned long long> orig_2(orig.begin(), orig.end());
+		std::set<unsigned long long, std::less<unsigned long long> > orig_2(
+				orig.begin(), orig.end());
 		for (auto x : orig_2) {
 			auto it = orig_2.upper_bound(x);
 			auto rs = (it == orig_2.end() ? NO : *it);
 			auto vtrs = vt.next(x);
 			if (rs != vtrs) {
 				std::cerr << "NEXT gives " << rs << " for original and " << vtrs
+						<< " on real for target " << x << "\n";
+				test_rs = false;
+				break;
+			}
+		}
+		std::set<unsigned long long, std::greater<unsigned long long> > orig_3(
+				orig.begin(), orig.end());
+		for (auto x : orig_3) {
+			auto it = orig_3.upper_bound(x);
+			auto rs = (it == orig_3.end() ? NO : *it);
+			auto vtrs = vt.prev(x);
+			if (rs != vtrs) {
+				std::cerr << "PREV gives " << rs << " for original and " << vtrs
 						<< " on real for target " << x << "\n";
 				test_rs = false;
 				break;
